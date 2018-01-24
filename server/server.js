5ef7159f3d565cf20e61a5c95c18c34736650518
suneelk14 <suneel.kesari14@github.com>
2018-01-24 10:52:49 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const _ = require('lodash') ;
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require ('bcryptjs') ;
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/Todo.js');
@@ -107,6 +108,26 @@ app.get('/users', (req, res)=>{
 app.get('/users/me', authenticate, (req, res)=>{
 res.send(req.user);
 });
+
+
+app.post('/users/login', (req, res)=>{
+  // var email = _.pick(req.body, ['email']);
+  // var password = _.pick(req.body, ['password']);
+  var body =  _.pick(req.body, ['email', 'password']);
+  var email = body.email ;
+  var password = body.password ;
+
+  User.findByCredentials(email, password).then((user)=>{
+    return user.generateAuthToken().then((token)=>{
+          res.header('x-auth', token).send(user);
+    });
+
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+
+}) ;
+
 
 app.listen(port, ()=>{
   console.log(`Started on port ${port}`);
